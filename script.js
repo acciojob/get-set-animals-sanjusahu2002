774b5415b647fb47819e8c12854b8f32d8b4afe5
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2025-01-29 10:49:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-//complete this code
-class Animal {}
+        class Animal{
+            constructor(species){
+                this.species = species;
 
-class Dog extends Animal {}
+            }
+            get getSpecies(){
+                return `${this.species}`;
+            }
 
-class Cat extends Animal {}
+            mekeSound(){
+                console.log(`The ${this.species} makes a sound`);
+            }
+        }
+        class Cat extends Animal{
+            constructor(species){
+                super(species);
+            }
+            purr(){
+                
+                console.log('purr');
+            }
+        }
+        class Dog extends Animal{
+            constructor(species){
+                super(species);
+            }
+            bark(){
+                console.log('woof')
+            }
+        }
+        const myCat = new Cat('Simeanse');
+        myCat.mekeSound();
+        myCat.purr();
+
 
 // Do not change the code below this line
 window.Animal = Animal;
